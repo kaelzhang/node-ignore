babc421b4a78204fa25740e518772cd2027eb63d
Kael Zhang <i+github@kael.me>
2018-08-09 13:52:09 +0800

    updates dev deps, fixes eslint 5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,10 @@
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  extends: 'airbnb-base',
+  // Uses `require.resolve` to support npm linked eslint-config
+  extends: require.resolve('eslint-config-ostai'),
   root: true,
-  parserOptions: {
-    sourceType: 'module'
-  },
   rules: {
-    semi: ['error', 'never'],
-
-    camelcase: ['off', 'never'],
-
-    'comma-dangle': ['error', 'never'],
-
-    'object-curly-spacing': ['error', 'never'],
-
-    'arrow-parens': ['error', 'as-needed'],
-
-    indent: ['error', 2, {
-      MemberExpression: 0
-    }],
-
-    'no-multi-spaces': ['error', {
-      ignoreEOLComments: true
-    }],
-
-    'no-extra-semi': 'error',
-
-    'space-before-function-paren': ['error', 'always'],
-
-    'no-use-before-define': ['error', {
-      functions: false
-    }],
-
-    'func-names': ['error', 'as-needed'],
-
-    // allow ++
-    'no-plusplus': 'off',
-
-    'space-unary-ops': ['error', {
-      words: true,
-      nonwords: true,
-      // a ++
-      // a --
-      // !a
-      // !!a
-      overrides: {
-        '!': false,
-        '!!': false
-      }
-    }],
-
-    'no-param-reassign': 'off',
-
     'no-underscore-dangle': ['error', {
       allowAfterThis: true,
       enforceInMethodNames: false,
@@ -60,12 +12,11 @@ module.exports = {
       allow: ['_rules', '_filter']
     }],
 
-    'no-return-assign': 'off',
+    'indent': ['error', 2, {
+      MemberExpression: 0,
 
-    'no-bitwise': 'off',
-
-    'no-confusing-arrow': 'off',
-
-    'no-mixed-operators': 'off'
+      // Eslint bug
+      ignoreComments: true
+    }]
   }
 }
